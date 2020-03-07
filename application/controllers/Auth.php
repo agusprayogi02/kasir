a71296393de718b5228d6cd9edbad0e36522f493
agus_prayogi <agusyogi600@gmail.com>
2020-03-07 09:24:05 +0700

    App CI Kasir

---
+++ application/controllers/Auth.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$data['title'] = "Halaman Login";
+		$this->load->view('templates/auth_header', $data);
+		$this->load->view('auth/login');
+		$this->load->view('templates/auth_footer');
+	}
+
+	public function registration()
+	{
+		$data['title'] = "Halaman Regitration";
+		$this->load->view('templates/auth_header', $data);
+		$this->load->view('auth/registration');
+		$this->load->view('templates/auth_footer');
+	}
+}
---
+++ application/views/auth/login.php
@@ -0,0 +1,45 @@
+<div class="container">
+  <!-- Outer Row -->
+  <div class="row justify-content-center">
+    <div class="col-lg-6 col-xl-7 col-md-8">
+      <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+          <!-- Nested Row within Card Body -->
+          <div class="row">
+            <div class="col-lg">
+              <div class="p-5">
+                <div class="text-center">
+                  <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                </div>
+                <form class="user">
+                  <div class="form-group">
+                    <input type="text" class="form-control form-control-user" name="email" id="exampleInputEmail" placeholder="Enter Email Address..." required>
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control form-control-user" name="password" id="exampleInputPassword" placeholder="Password" required>
+                  </div>
+                  <div class="form-group">
+                    <div class="custom-control custom-checkbox small">
+                      <input type="checkbox" class="custom-control-input" id="customCheck">
+                      <label class="custom-control-label" for="customCheck">Remember Me</label>
+                    </div>
+                  </div>
+                  <button href="index.html" class="btn btn-primary btn-user btn-block">
+                    Login
+                  </button>
+                </form>
+                <hr>
+                <div class="text-center">
+                  <a class="small" href="forgot-password.html">Forgot Password?</a>
+                </div>
+                <div class="text-center">
+                  <a class="small" href="auth/registration">Create an Account!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
